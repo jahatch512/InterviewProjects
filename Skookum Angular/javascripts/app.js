dd3ba01f4b68328094b9f46b9c0b7fa611edf700
jahatch512 <ja.hatch512@gmail.com>
2016-08-10 12:26:30 -0600

    connected to HackerNews API to get the top news stories

--- Skookum Angular/javascripts/app.js
+++ Skookum Angular/javascripts/app.js
@@ -19,8 +19,26 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
   });
 }).controller("homeController", function($scope){
   $scope.message = "Welcome to James World";
-}).controller("newsController", function($scope){
+}).controller("newsController", function($scope, $http){
   $scope.sources = ["TechCrunch", "HackerNews"];
+  $http.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+  .then(function(data){
+    console.log("first ajax");
+
+    $http.get("https://hacker-news.firebaseio.com/v0/item/"+
+    data.data[0]+
+    ".json?print=pretty").then(
+      function(data2){
+        console.log("second ajax call");
+
+        console.log(data2.data.url);
+
+        $scope.title = data2.data.title;
+        $scope.text = data2.data.text;
+        $scope.url = data2.data.url;
+      }
+    )
+  });
 }).controller("weatherController", function($scope){
   $scope.weather = "Sunny";
 }).controller("sportsController", function($scope){
