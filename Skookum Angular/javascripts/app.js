c5a0d1e12aec69cd5ca5a16ba8337299bb05ef73
jahatch512 <ja.hatch512@gmail.com>
2016-08-25 02:27:43 -0600

    added music and weather pages

--- Skookum Angular/javascripts/app.js
+++ Skookum Angular/javascripts/app.js
@@ -1,7 +1,10 @@
 
 var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
 .config(function($routeProvider){
-  $routeProvider.when("/news", {
+  $routeProvider.when("/", {
+    templateUrl: "../views/home.html",
+    controller: "newsController"
+  }).when("/news", {
     templateUrl: "../views/news.html",
     controller: "newsController"
   }).when("/weather", {
@@ -27,17 +30,17 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
         if (data2.data.title){
           $scope.title1 = data2.data.title;
         } else {
-          $scope.title1 = "no title";
+          $scope.title1 = "(no title available)";
         }
         if (data2.data.text){
           $scope.text1 = data2.data.text;
         } else {
-          $scope.text1 = "no text";
+          $scope.text1 = "(no text available)";
         }
         if (data2.data.url){
           $scope.url1 = data2.data.url;
         } else {
-          $scope.url1 = "no url";
+          $scope.url1 = "(no url available)";
         }
       }
     );
@@ -45,18 +48,17 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
 
   $http.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=popular&apiKey=e464350201154d5bbeb3ee037abf09fb")
   .then(function(data2){
-    console.log("second request success");
     console.log(data2.data.articles[0]);
 
     if (data2.data.articles[0].title){
       $scope.title2 = data2.data.articles[0].title;
     } else {
-      $scope.title2 = "no title";
+      $scope.title2 = "(no title available)";
     }
     if (data2.data.articles[0].description){
       $scope.description = data2.data.articles[0].description;
     } else {
-      $scope.description = "no description";
+      $scope.description = "(no description available)";
     }
     if (data2.data.articles[0].url){
       $scope.url2 = data2.data.articles[0].url;
@@ -66,8 +68,34 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
   }
   );
 
-}).controller("weatherController", function($scope){
+}).controller("weatherController", function($scope, $http){
   $scope.weather = "Sunny";
+  $http.get("http://api.wunderground.com/api/Your_Key/conditions/q/CA/San_Francisco.json")
+  .then(function(data){
+    console.log("second request success");
+    console.log(data);
+
+    if (data.data.articles[0].title){
+      $scope.title = data.data.articles[0].title;
+    } else {
+      $scope.title = "(no title available)";
+    }
+    if (data.data.articles[0].description){
+      $scope.description = data.data.articles[0].description;
+    } else {
+      $scope.description = "(no description available)";
+    }
+    if (data.data.articles[0].url){
+      $scope.url = data.data.articles[0].url;
+    } else {
+      $scope.url = "no url";
+    }
+    if (data.data.articles[0].urlToImage){
+      $scope.image = data.data.articles[0].urlToImage;
+    } else {
+      $scope.image = "(no image available)";
+    }
+  });
 }).controller("sportsController", function($scope, $http){
   $scope.sportSources = ["ESPN", "Bleacher Report"];
   $http.get("https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=e464350201154d5bbeb3ee037abf09fb")
@@ -78,12 +106,12 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
     if (data.data.articles[0].title){
       $scope.title = data.data.articles[0].title;
     } else {
-      $scope.title = "no title";
+      $scope.title = "(no title available)";
     }
     if (data.data.articles[0].description){
       $scope.description = data.data.articles[0].description;
     } else {
-      $scope.description = "no description";
+      $scope.description = "(no description available)";
     }
     if (data.data.articles[0].url){
       $scope.url = data.data.articles[0].url;
@@ -93,7 +121,7 @@ var JamesWorld = angular.module('JamesWorld', ["ngRoute"])
     if (data.data.articles[0].urlToImage){
       $scope.image = data.data.articles[0].urlToImage;
     } else {
-      $scope.image = "no image";
+      $scope.image = "(no image available)";
     }
   });
 
